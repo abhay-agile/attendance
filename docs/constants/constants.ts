2414c446147756bb69dfe9ccb7f5e37d8bdaad32
Abhay <abhay.n@agileinfoways.com>
2024-09-26 13:44:20 +0530

    feat:added constant file

---
+++ docs/constants/constants.ts
@@ -0,0 +1,58 @@
+import { ILateEntry, IMoneyEntry, IMoneyUsage, IUserData } from "./interface";
+
+export const userData: IUserData[] = [
+  {
+    id: '1',
+    name: 'Abhay Dalsaniya',
+    email: 'abhay@yopmail.com',
+    password: 'Abhay@001',
+    image: 'assets/images/abhay.png',
+    seteled: 0
+  },
+  {
+    id: '2',
+    name: 'Tarak Shah',
+    email: 'tarak@yopmail.com',
+    password: 'Tarak@001',
+    image: 'assets/images/tarak.png',
+    seteled: 0
+  },
+  {
+    id: '3',
+    name: 'Vipul Padiyar',
+    email: 'vipul@yopmail.com',
+    password: 'Vipul@001',
+    image: 'assets/images/vipul.png',
+    seteled: 0
+  },
+  {
+    id: '4',
+    name: 'Saurav Sapariya',
+    email: 'saurav@yopmail.com',
+    password: 'Saurav@001',
+    image: 'assets/images/saurav.png',
+    seteled: 0
+  },
+  {
+    id: '5',
+    name: 'Vaibhavi Rathod',
+    email: 'vaibhavi@yopmail.com',
+    password: 'Vaibhavi@001',
+    image: 'assets/images/vaibhavi.png',
+    seteled: 0
+  },
+  {
+    id: '6',
+    name: 'Rajvi Kapadiya',
+    email: 'rajvi@yopmail.com',
+    password: 'Rajvi@001',
+    image: 'assets/images/rajvi.png',
+    seteled: 0
+  },
+];
+
+export const lateEntry: ILateEntry[] = [];
+
+export const moneyEntry: IMoneyEntry[] = [];
+
+export const moneyUsage: IMoneyUsage[] = [];
